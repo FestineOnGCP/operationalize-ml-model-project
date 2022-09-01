4b8e508df3d390d2b78219730aeba9706fcc7ead
Festus Udomoh <festusudomoh@Festuss-MacBook-Pro.local>
2022-09-01 19:30:10 +0100

    run kuberentes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=festinedevops/operationalize-ml-model
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-pod --port=80 --image=$dockerpath:v1
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-pod 8000:80
